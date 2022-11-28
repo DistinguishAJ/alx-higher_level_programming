5169cfa081d9e82a84a050e9caa0e925944fbd74
DistinguishAJ <distinguishjosiaha@gmail.com>
2022-11-28 11:15:45 -0800

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+First_3_letter = word[:3]
+Last_2_letter = word[-2:]
+Middle_word = word[1:-1]
+print("First 3 letters: {}".format(First_3_letter))
+print("Last 2 letters: {}".format(Last_2_letter))
+print("Middle word: {}".format(Middle_word))

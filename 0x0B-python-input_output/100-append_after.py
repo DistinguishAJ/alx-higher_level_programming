e41d885cef4c5f2ae1d63045491c1fcb02a45daa
DistinguishAJ <distinguishjosiaha@gmail.com>
2023-01-10 17:13:40 -0800

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+ a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in i
+file.
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    text = ""
+    with open(filename) as gem:
+        for line in gem:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as j:
+        j.write(text)
